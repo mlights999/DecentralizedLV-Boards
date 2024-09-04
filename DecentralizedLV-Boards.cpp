1071c41eaa6e836008f657c9559074d645ff4ed6
Matthew Panizza <mlights999@gmail.com>
2024-09-04 19:19:48 -0400

    Initial Submodule Test

---
+++ DecentralizedLV-Boards.cpp
@@ -0,0 +1,7 @@
+#include "DecentralizedLV-Boards.h"
+#include "Particle.h"
+
+void testClass(){
+    pinMode(D7, OUTPUT);
+    digitalWrite(D7, HIGH);
+}
---
+++ DecentralizedLV-Boards.h
@@ -0,0 +1,9 @@
+#ifndef DECENTRALIZED_LV_BOARDS_H
+#define DECENTRALIZED_LV_BOARDS_H
+
+#include "Particle.h"
+
+void testClass();
+
+
+#endif
