e06a2a15bdddf3eecb69689abd4433893ab53ed1
reecedobro <reecedobro@gmail.com>
2026-04-06 18:44:09 -0400

    comment

--- DecentralizedLV-Boards.cpp
+++ DecentralizedLV-Boards.cpp
@@ -270,15 +270,8 @@ void IBOOSTER_CAN::initialize(){
 void IBOOSTER_CAN::receiveCANData(LV_CANMessage msg){
     if(msg.addr == boardAddress){
         boardDetected = true;
-<<<<<<< Updated upstream
-        //Reece: fully pressed my ass. this needs to be fixed. maybe monitor when brakes are being fully pressed by if it is zero and the number was increasing? further testing is needed. 
-        static int brakeMin = 0x50;
-        static int brakeMax = 0xC0;
-        //msg.byte5 ranges from 0x50 (fully released) to 0xC0 (fully pressed)
-=======
 
         //msg.byte5 ranges from 0x50 (fully released) to 0xC0 (fully pressed)  //Reece: fully pressed my ass. this needs to be fixed. maybe monitor when brakes are being fully pressed by if it is zero and the number was increasing? further testing is needed. 
->>>>>>> Stashed changes
         static int brakeMin = 0x50;
         static int brakeMax = 0xC0;
         int brakeVal = constrain(msg.byte5, brakeMin, brakeMax);
